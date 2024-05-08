0cc61de052199e67714a9d0bcfce670572605397
MadihaAli1 <madihmali57@gmail.com>
2024-05-09 04:04:13 +0500

    add projects

---
+++ project05_word_counter/main.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+//importing inquirer and chalk packages;
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Prompt the user to enter a sentence;
+//Using Object array;
+let answers = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter a Sentence:",
+  },
+]);
+
+//Trimming the sentence and splitting it into words based on "spaces";
+let words = answers.sentence.trim().split(" ");
+
+//Analysis of user input sentence;
+console.log(chalk.bold("\n- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n- Word Count: ${chalk.bold.red(words.length)}\n`));
+
